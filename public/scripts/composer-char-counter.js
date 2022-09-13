59b43955acb52beeb06b9400c2aba5c8d8544a3f
plettboy <ryan.plett@yahoo.com>
2022-09-13 22:31:39 +0000

    character counter added

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  //
+  $("#tweet-text").on('input', (event) => {
+    //use jQuery to target the counter class
+     const counter = $(".counter")
+//assign currentCount to the event character length, through the DOM
+    let currentCount = event.target.value.length;
+    let totalChars = 140;
+    let charsLeft = totalChars - currentCount;
+    counter.val(charsLeft);
+    //not toggle, toggle turns on off, use add because it turns on the class and keeps it on
+    if (charsLeft < 0) {
+     $( ".counter" ).addClass( "counterRed" )
+    } else {
+      $( ".counter" ).removeClass( "counterRed" )
+    }
+  })
+  
+
+});
